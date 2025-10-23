06bbb4bf67aef5f1eec708ec2220e8aa9d5762a1
24jy0217 Taniguchi Kanaho <24jy0217@jec.ac.jp>
2025-10-23 10:48:49 +0900

    Helloを10回繰り返す

--- src/project2/HelloWorld.java
+++ src/project2/HelloWorld.java
@@ -3,7 +3,9 @@ package project2;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Hello?");
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello, Hello?");
+		}
 	}
 
 }
